a33248bb4dd7c9f6f684dedd3982b149a66c91d6
Ahana Ghosh <ahghosh@microsoft.com>
2019-10-29 18:25:27 -0700

    Creating and styling components

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import EventList from './EvenList';
+
+export default class App extends React.Component{
+  render(){
+      return(
+          <Eventlist/>
+      );
+  }
+}
+
